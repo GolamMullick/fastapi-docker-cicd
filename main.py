82d8ddeb360122809fc613db61f6cb1ddccdf6b6
Golam Mullick <fahadmullick89@gmail.com>
2025-06-08 23:39:12 +1000

    postgress_added

--- main.py
+++ main.py
@@ -177,10 +177,12 @@ def stop_worker():
     processing = False
     return {"status": "worker stopping"}
 
+from sqlalchemy import text
+
 @app.get("/db-health")
 def db_health(db: Session = Depends(get_db)):
     try:
-        db.execute("SELECT 1")
+        db.execute(text("SELECT 1"))
         return {"status": "ok", "message": "Postgres Database connection is healthy."}
     except Exception as e:
         return {"status": "error", "message": str(e)}
